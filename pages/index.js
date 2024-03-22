01e3d65b83ca35a0347e76705aeaee87ab5c88d2
marcusdela <123431336+marcusdela@users.noreply.github.com>
2024-03-21 23:11:38 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="stockr.ai" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          S&P500 speculation made easy <code>pages/index.js</code>
         </p>
       </main>
 
